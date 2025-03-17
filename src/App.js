ecfee71022ee1e2101b483a0612bb89809ec71fe
leo-parkd <byeonghunpark@snu.ac.kr>
2025-03-17 15:59:58 +0900

    feat: add kakao map api

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main';
-import ChatMain from './pages/ChatMain';
+import ProfileMain from './pages/ChatMain';
 import ChatDetail from './pages/ChatDetail';
 import ProfileEdit from './pages/ProfileEdit';
 import PasswordChange from './pages/PasswordChange';
 import MemberWithdrawal from './pages/MemberWithdrawal';
 import MapPage from './pages/MapPage';
-import ChatService from './pages/ChatService';
+import ChatMain from './pages/ChatService';
 import PetInfo from './pages/PetInfo';
 import PetEdit from './pages/PetEdit';
 import PetRegister from './pages/PetRegister';
@@ -25,13 +25,13 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/main" element={<Main />} />
-              <Route path="/profile" element={<ChatMain />} />
+              <Route path="/profile" element={<ProfileMain />} />
               <Route path="/chat/:id" element={<ChatDetail />} />
               <Route path="/profile-edit" element={<ProfileEdit />} />
               <Route path="/password-change" element={<PasswordChange />} />
               <Route path="/profile/withdrawal" element={<MemberWithdrawal />} />
               <Route path="/map" element={<MapPage />} />
-              <Route path="/chat-service" element={<ChatService />} />
+              <Route path="/chat-main" element={<ChatMain />} />
               <Route path="/pet-info" element={<PetInfo />} />
               <Route path="/pet-edit" element={<PetEdit />} />
               <Route path="/pet-register" element={<PetRegister />} />
