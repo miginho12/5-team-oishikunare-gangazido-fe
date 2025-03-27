18917fadf36e46e87301e3754595a7c1e8369bc0
Gdugboy <gdugboy@gmail.com>
2025-03-27 14:00:10 +0900

    feat: 마커 등록 시 크로스헤어 변경(찍히는 위치변경 필요)

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1299,25 +1299,11 @@ function MapPage() {
         {isCenterMode && (
           <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-[60%] z-10 pointer-events-none">
             <div className="relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-10 w-10 text-amber-800"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
+              <img
+                src="/images/cross-mark.png"
+                alt="중앙 마커"
+                className="w-4 h-4" // tailwind 기준 크기 변경 10 : 40px?
+              />
               <div className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 whitespace-nowrap bg-black bg-opacity-70 px-2 py-1 rounded text-white text-xs">
                 지도를 움직여 위치 조정
               </div>
