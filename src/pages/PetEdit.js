51533f04baa1e708ef87bf279c3c70f70b35bb10
Myungbeem Kim <128389329+mbeankong@users.noreply.github.com>
2025-03-27 19:07:55 +0900

    chore: 반려견 예외처리 텍스트 수정

--- src/pages/PetEdit.js
+++ src/pages/PetEdit.js
@@ -264,6 +264,10 @@ function PetEdit() {
       case 'not_found_user':
         alert('사용자를 찾을 수 없습니다.');
         break;
+
+      case 'not_found_pet': 
+        alert('반려견 정보를 찾을 수 없습니다.');
+        break;
   
       case 'internal_server_error':
         alert('서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
