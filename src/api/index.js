aeb9ca5f517a2273ee35c14ddfa817ebf842accb
leo-parkd <byeonghunpark@snu.ac.kr>
2025-03-27 13:16:50 +0900

    feat: 배포 환경 설정
    API_BASE_URL seceret key 추가 및 index.js 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
+  baseURL: window._env_?.API_BASE_URL || 'http://localhost:8080',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
