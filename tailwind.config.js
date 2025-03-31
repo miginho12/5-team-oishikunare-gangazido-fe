eee19a16ed0ea9ff143a69141f5024954d9573ce
Gdugboy <gdugboy@gmail.com>
2025-03-31 23:03:35 +0900

    feat: 마커 등록 모달창 디자인 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-up': 'fadeUp 0.3s ease-out',
+      },
+      keyframes: {
+        fadeUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)' // 아래에서 위로
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
